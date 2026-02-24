fbaaf387fad2e33c17d54c23e3f0df80488faf82
DanielBramlett <daniel.bramlett@timothyministry.com>
2026-02-24 14:23:59 -0500

    First Phase

--- source/config.js
+++ source/config.js
@@ -5,5 +5,12 @@ export const gameConfig = {
     width: 800,
     height: 600,
     backgroundColor: '#2d2d2d',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: { y: 0 }
+        }
+    },
     scene: GameScene
 };
